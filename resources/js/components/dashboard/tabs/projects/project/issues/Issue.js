7109d402c2a742082d29221b6ec80d893d7a7964
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-01-29 13:44:16 +0800

    Refactored Issue component

--- resources/js/components/dashboard/tabs/projects/project/issues/Issue.js
+++ resources/js/components/dashboard/tabs/projects/project/issues/Issue.js
@@ -2,6 +2,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle, faExclamationCircle, faEllipsisH, faPen, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 import useIssue from './useIssueHook';
+import OpenIssue from './issue/OpenIssue';
+import ClosedIssue from './issue/ClosedIssue';
 
 
 const Issue = ({ issue }) => {
@@ -19,7 +21,7 @@ const Issue = ({ issue }) => {
         if (e.key === 'Enter') {
           updateIssue({ id: issue.id, issueName, assigneeId, issueStatus });
           setIsSaved(true);
-        } 
+        }
       }}
     >
       <div className="w-full min-h-[110px] flex justify-between items-center py-4 px-8">
@@ -38,87 +40,32 @@ const Issue = ({ issue }) => {
         </div>
       </div>
 
-      {issueStatus === 'open' && isCollapsed && (
-        <div className="w-[460px] mx-auto mb-3">
-
-          <div className="w-full h-[40px] bg-light-gray mx-auto rounded-md mb-3">
-            <div className="flex justify-between items-center w-full h-full px-4">
-              <input
-                className="flex flex-1 bg-light-gray outline-none custom-placeholder"
-                type="text"
-                value={issueName}
-                onChange={e => {
-                  setIssueName(e.target.value);
-                  setIsSaved(false);
-                }}
-              />
-              <div className="w-[30px] h-[30px] flex justify-center items-center ml-2">
-                <FontAwesomeIcon icon={faPen} color="#4F4F4F" />
-              </div>
-            </div>
-          </div>
-          <div className="w-full h-[40px] mb-3 flex justify-between">
-            <div className="w-1/2 bg-light-gray flex rounded-md">
-              <div className="w-full flex items-center px-4">
-                <input
-                  className="w-full bg-light-gray outline-none custom-placeholder"
-                  type="text"
-                  name="assignee"
-                  id="assignee"
-                  placeholder="Assignee"
-                />
-                <div className="w-[30px] h-[30px] flex justify-center items-center ml-2">
-                  <FontAwesomeIcon icon={faAngleDown} color="#4F4F4F" />
-                </div>
-              </div>
-            </div>
-            <div className="w-1/2 bg-light-gray flex rounded-md ml-3">
-              <div className="w-full flex items-center px-4">
-                <input
-                  className="w-full bg-light-gray outline-none custom-placeholder"
-                  type="text"
-                  name="status"
-                  id="status"
-                  placeholder="Status"
-                />
-                <div className="w-[30px] h-[30px] flex justify-center items-center ml-2">
-                  <FontAwesomeIcon icon={faAngleDown} color="#4F4F4F" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <button
-            className="bg-purple w-full h-[50px] text-[18px] text-white font-medium rounded-md"
-            onClick={() => {
+      {isCollapsed && (
+        issueStatus === 'open' ? (
+          <OpenIssue
+            data={{ issueName, assigneeId, issueStatus }}
+            onIssueNameChange={e => {
+              setIssueName(e.target.value);
+              setIsSaved(false);
+            }}
+            onSave={() => {
               updateIssue({ id: issue.id, issueName, assigneeId, issueStatus });
               setIsSaved(true);
             }}
-          >
-            {isSaved ? 'Saved!' : 'Save'}
-          </button>
-
-        </div>
-      )}
-
-      {issueStatus === 'closed' && isCollapsed && (
-        <div className="w-[460px] mx-auto mb-3">
-          <button
-            className="bg-purple w-full h-[50px] text-[18px] text-white font-medium rounded-md"
-            onClick={null}
-          >
-            Re-open
-          </button>
-        </div>
+            isSaved={isSaved}
+          />
+        ) : (
+          <ClosedIssue
+            onReOpenIssue={null}
+          />
+        )
       )}
 
       <div
         className="flex justify-center items-center w-full h-[10px] cursor-pointer hover:bg-light-gray"
         onClick={() => setIsCollapsed(!isCollapsed)}
       >
-        <FontAwesomeIcon
-          icon={faEllipsisH}
-          color="#6816BB"
-        />
+        <FontAwesomeIcon icon={faEllipsisH} color="#6816BB" />
       </div>
     </div>
   );
