438e47d8ce4f1d7d7bd43a4482e600be12b88c33
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-02-11 20:59:27 +0800

    UI fixes in profile tab

--- resources/js/components/dashboard/tabs/Profile.js
+++ resources/js/components/dashboard/tabs/Profile.js
@@ -14,7 +14,7 @@ const Profile = () => {
       </p>
       
       <div>
-        <div className="flex flex-col py-10 px-5 justify-center items-center border-l-[5px] border-l-purple shadow-lg">
+        <div className="flex flex-col py-10 px-5 md:w-[500px] md:mx-auto justify-center items-center border-l-[5px] border-l-purple shadow-lg">
           <div className="relative w-[200px] h-[200px] rounded-[50%] mb-5">
             <img
               className="w-full h-full object-cover rounded-[50%]"
