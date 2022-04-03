095dd23536106247f0433440d40201e1ce82b8f2
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-04-04 05:05:00 +0800

    fixed Friends Tab sub tabs UI overflowing or scrollable

--- resources/js/tabs/Friends/index.js
+++ resources/js/tabs/Friends/index.js
@@ -14,23 +14,39 @@ const Friends = () => {
 
         <SearchFriend />
 
-        <div className="inline-block lg:flex lg:justify-center w-full border border-y-gray border-x-white my-[40px] py-2 overflow-x-auto">
+        <div className="inline-block lg:flex lg:justify-center w-full border
+        border-y-gray border-x-white my-[40px] py-2 overflow-x-hidden"
+        >
           <div
-            className={`flex justify-center items-center w-full lg:w-fit mr-16 cursor-pointer ${currentTab === tabs[0] ? 'opacity-100' : 'opacity-50'}`}
+            className={`flex justify-center items-center w-full lg:w-fit
+            mr-16 cursor-pointer ${currentTab === tabs[0] ? 'opacity-100' : 'opacity-50'}`}
             onClick={() => setCurrentTab(tabs[0])}
           >
-            <span className="inline-block font-medium text-[25px]">All friends</span>
-            <div className="flex justify-center items-center w-[30px] h-[30px] bg-gray rounded-full ml-3">
-              <span className="text-white text-[13px] font-semibold">{friends.length}</span>
+            <span className="inline-block font-medium text-[25px]">
+              All friends
+            </span>
+            <div className="flex justify-center items-center w-[30px] h-[30px] bg-gray
+              rounded-full ml-3"
+            >
+              <span className="text-white text-[13px] font-semibold">
+                {friends.length}
+              </span>
             </div>
           </div>
           <div
-            className={`flex justify-center items-center w-full lg:w-fit cursor-pointer ${currentTab === tabs[1] ? 'opacity-100' : 'opacity-50'}`}
+            className={`flex justify-center items-center w-full lg:w-fit cursor-pointer
+            ${currentTab === tabs[1] ? 'opacity-100' : 'opacity-50'}`}
             onClick={() => setCurrentTab(tabs[1])}
           >
-            <span className="inline-block font-medium text-[25px]">Friend requests</span>
-            <div className="flex justify-center items-center w-[30px] h-[30px] bg-gray rounded-full ml-3">
-              <span className="text-white text-[13px] font-semibold">{friendRequests.length}</span>
+            <span className="inline-block font-medium text-[25px]">
+              Friend requests
+            </span>
+            <div className="flex justify-center items-center w-[30px] h-[30px] bg-gray
+              rounded-full ml-3"
+            >
+              <span className="text-white text-[13px] font-semibold">
+                {friendRequests.length}
+              </span>
             </div>
           </div>
         </div>
