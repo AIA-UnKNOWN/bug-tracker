0eb586efa314157f5279fb87555857a4d2fcb469
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-04-05 14:57:36 +0800

    fixed an issue where friend requests number doesnt show immediately

--- resources/js/tabs/Friends/hook.js
+++ resources/js/tabs/Friends/hook.js
@@ -1,19 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import useFriendRequests from './FriendRequests/hook';
 import FriendsList from './FriendsList';
 import FriendRequests from './FriendRequests';
 
 const useFriends = () => {
+  const { getFriendRequests } = useFriendRequests();
   const friends = useSelector(state => state.friends.friends);
   const friendRequests = useSelector(state => state.friends.friendRequests);
   const tabs = ['all friends', 'friend requests'];
   const [currentTab, setCurrentTab] = useState(tabs[0]);
+  
+  useEffect(() => {
+    getFriendRequests();
+  }, []);
 
   const renderPage = tab => {
     switch (tab) {
       case tabs[1]:
         return (<FriendRequests data={friendRequests} />);
-        break;
       default:
         return (<FriendsList data={friends} />);
     }
